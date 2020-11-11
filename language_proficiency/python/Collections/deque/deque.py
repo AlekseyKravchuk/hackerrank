501361c9c836f3f1c14bc682b3a233cfb10e3b51
Aleksey Kravchuk <aleksey-kravchuk@outlook.com>
2020-11-11 18:50:19 +0300

    Added deque.py

---
+++ language_proficiency/python/Collections/deque/deque.py
@@ -0,0 +1,19 @@
+""" https://www.hackerrank.com/challenges/py-collections-deque/problem """
+
+from collections import deque
+
+
+def main():
+    q = deque()
+    for i in range(int(input())):
+        line = [x for x in input().split()]
+        if len(line) == 2:
+            exec(f'q.{line[0]}({line[1]})')
+        elif len(line) == 1:
+            exec(f'q.{line[0]}()')
+    for elm in q:
+        print(elm, end=' ')
+
+
+if __name__ == '__main__':
+    main()
